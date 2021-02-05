ff475910d376199ee81e90a0a45a029aad5d2ea8
Tristan Da Costa Pinto <tristandacostapinto@gmail.com>
2021-02-05 14:32:33 +0100

    Role + User

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -13,11 +13,16 @@ class ComposerAutoloaderInita26f97b2fb4e50e2fa6373587ecb538d
         }
     }
 
+    /**
+     * @return \Composer\Autoload\ClassLoader
+     */
     public static function getLoader()
     {
         if (null !== self::$loader) {
             return self::$loader;
         }
+
+        require __DIR__ . '/platform_check.php';
 
         spl_autoload_register(array('ComposerAutoloaderInita26f97b2fb4e50e2fa6373587ecb538d', 'loadClassLoader'), true, true);
         self::$loader = $loader = new \Composer\Autoload\ClassLoader();
@@ -25,7 +30,7 @@ class ComposerAutoloaderInita26f97b2fb4e50e2fa6373587ecb538d
 
         $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION') && (!function_exists('zend_loader_file_encoded') || !zend_loader_file_encoded());
         if ($useStaticLoader) {
-            require_once __DIR__ . '/autoload_static.php';
+            require __DIR__ . '/autoload_static.php';
 
             call_user_func(\Composer\Autoload\ComposerStaticInita26f97b2fb4e50e2fa6373587ecb538d::getInitializer($loader));
         } else {
